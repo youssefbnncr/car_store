24cbd82f92dd07e6419436e184842e0cc7fa26d7
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-08-14 13:16:28 +0100

    setup states and cities tables

--- database/migrations/2025_08_14_112255_create_states_table.php
+++ database/migrations/2025_08_14_112255_create_states_table.php
@@ -4,16 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('states', function (Blueprint $table) {
+        Schema::create("states", function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("name", 45);
         });
     }
 
@@ -22,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('states');
+        Schema::dropIfExists("states");
     }
 };
--- database/migrations/2025_08_14_112308_create_cities_table.php
+++ database/migrations/2025_08_14_112308_create_cities_table.php
@@ -4,16 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('cities', function (Blueprint $table) {
+        Schema::create("cities", function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId("state_id")->constrained("states");
+            $table->string("name", 45);
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cities');
+        Schema::dropIfExists("cities");
     }
 };
