e98bfad5a6196b93f6b3817673e6d6ba5e56be30
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-08-14 12:57:11 +0100

    setup new tabkes

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\LoginController;
 
-Route::view('/', 'index');
-
-Route::get('/car/search', [CarController::class, 'search'])->name('car.search');
-Route::resource('car', CarController::class);
-
-Route::get('/signup', [SignupController::class, 'create'])->name('signup');
-Route::get('/login',[LoginController::class, 'create'])->name('login');
+Route::view("/", "index");
+Route::get("/car/search", [CarController::class, "search"])->name("car.search");
+Route::resource("car", CarController::class);
+Route::get("/signup", [SignupController::class, "create"])->name("signup");
+Route::get("/login", [LoginController::class, "create"])->name("login");
